4e1a24d7780b04d7dda4271ee25e22e206f75720
Nalanda <Nalanda@DESKTOP-UMRTKDR>
2020-03-30 17:56:47 -0400

    Task one - Program 2 and 3

---
+++ src/taskOne/Program2.java
@@ -0,0 +1,10 @@
+package taskOne;
+import java.util.Scanner;
+public class Program2 {
+public static void main(String args[]) {
+	Scanner sc = new Scanner(System.in);
+	System.out.print("Enter the required number=");
+	int number = sc.nextInt();
+	System.out.println("Given number is: "+number);
+}
+}
---
+++ src/taskOne/Program3.java
@@ -0,0 +1,14 @@
+package taskOne;
+
+import java.util.Scanner;
+public class Program3 {
+public static void main(String args[]) {
+	Scanner sc = new Scanner(System.in);
+	System.out.print("Enter first number:");
+	int a =sc.nextInt();
+	System.out.print("Enter the second number:");
+	int b=sc.nextInt();
+	int z=a+b;
+	System.out.println("Result:" +z);
+}
+}
