f6f8c8b2cee348d48d9a71fe115c6d727ec81eed
Nalanda <Nalanda@DESKTOP-UMRTKDR>
2020-03-30 17:46:50 -0400

    Program 1 - Task One

---
+++ src/taskOne/Program1.java
@@ -0,0 +1,15 @@
+package taskOne;
+
+public class Program1 {
+
+	public static void main(String[] args) {
+		int a = 1;
+		int b = 2;
+		int temp = 0;
+		temp = a;
+		a = b;
+		b = temp;
+		System.out.println("a=" + a + "\nb=" + b);
+	}
+
+}
